ec54a588d7e8813f2a32e440eb8254375c0d7d28
Eric <eric.yanez50@qmail.cuny.edu>
2022-07-15 17:12:23 -0400

    commit tree fc03d8197b22d69766fa3be0686d7ffd42a50b83

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("eric")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+greet_with_default "Naureen"
+
+def add(num1 ,num2)
+    num1 + num2
+end
+add(1,2)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+     number / 2
+     end
+
